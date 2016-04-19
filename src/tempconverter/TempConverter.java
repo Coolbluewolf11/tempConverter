c635b75068c902b520ea6f2375045c066933b6e8
Tim <Tim@10.30.17.50>
2016-04-19 13:52:22 -0500

    now with options

--- src/tempconverter/TempConverter.java
+++ src/tempconverter/TempConverter.java
@@ -16,11 +16,18 @@ public class TempConverter {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        String input = JOptionPane.showInputDialog(null, "What is the temp in Farenheit");
-        double Far = Double.parseDouble(input);
-        JOptionPane.showMessageDialog(null, "The temp in Celsuis is: " + Far);
-    }
-    
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        
+        for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+            if ("Nimbus".equals(info.getName())) {
+                javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                break;
+            }
+        }
+        
 }
